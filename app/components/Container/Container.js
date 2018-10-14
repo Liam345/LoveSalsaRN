f254decaef611a9aaddf58412edeb1a5b604b98d
Liam Banga <liam205@outlook.com>
2018-10-14 19:08:38 +1100

    Added basic container

---
+++ app/components/Container/Container.js
@@ -0,0 +1,14 @@
+import React from "react";
+import propTypes from "prop-types";
+import { View } from "react-native";
+import styles from "./styles";
+
+const Container = ({ children }) => (
+  <View style={styles.container}>{children}</View>
+);
+
+Container.propTypes = {
+  children: propTypes.element
+};
+
+export default Container;
---
+++ app/screens/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StatusBar } from "react-native";
+import { Container } from "../components/Container";
+//import { Logo } from "../components/Logo";
+
+export default () => (
+  <Container>
+    <Text>Hey</Text>
+  </Container>
+);
